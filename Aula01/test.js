41f2e2f7271892cd9dcc4bbfcb5c80b96b5c969e
Dev Jeff Alves <jeff-alves@outlook.com>
2024-07-01 11:46:07 -0300

    up

--- Aula01/test.js
+++ Aula01/test.js
@@ -1,10 +1,9 @@
 const rd = require('readline-sync')
 let name = rd.question('What is your name: ')
-let var1 = ['test1.js','test2.js']
+let var1 = ['test1','test2']
 let doc = rd.keyInSelect(var1, `${name}, what document do you want to open:`)
 function info(x1) {
-    let {text} = require(`./${var1[x1]}`)
+    let {text} = require(`./${var1[x1]}.js`)
     return text
 }
-
 console.log(info(doc))
