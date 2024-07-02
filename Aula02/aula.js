df14881ab36d12fa298f31eb913f8b1379ce4f7e
Dev Jeff Alves <jeff-alves@outlook.com>
2024-07-02 11:37:10 -0300

    y

--- Aula02/aula.js
+++ Aula02/aula.js
@@ -1,16 +1,17 @@
 const rd = require('readline-sync')
-/*
+/* 1
 const num1 = Number(rd.question('incira um numero: '))
 const num2 = Number(rd.question('incira outro numero: '))
 console.log(num1 + num2)
 */
-/*
+/* 2
 nota1 = Number(rd.question('insira a primeira nota: '))
 nota2 = Number(rd.question('insira a segunda nota: '))
 nota3 = Number(rd.question('insira a terceira nota: '))
 avrg = Math.floor((nota1+nota2+nota3)/3)
 console.log(`A média das notas é ${avrg}.`)
 */
+/* 3
 num1 = Number(rd.question('Insira um numero: '))
 num2 = Number(rd.question('Insira outro numero: '))
 if (num1 === num2){
@@ -27,3 +28,9 @@ if (comp==null){
 } else {
     console.log(`O numero ${num1} ${comp} ${num2}`)
 }
+*/
+/* 4
+racas=["linguica","bitbull","golden"]
+num=rd.question("escoloha um numero de 1 a 3: ")
+console.log(racas[num-1])
+*/
