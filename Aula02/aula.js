068fe22a16be81529d36631012111ccc3eb1f9ca
Dev Jeff Alves <jeff-alves@outlook.com>
2024-07-02 10:35:03 -0300

    y

--- Aula02/aula.js
+++ Aula02/aula.js
@@ -4,8 +4,26 @@ const num1 = Number(rd.question('incira um numero: '))
 const num2 = Number(rd.question('incira outro numero: '))
 console.log(num1 + num2)
 */
+/*
 nota1 = Number(rd.question('insira a primeira nota: '))
 nota2 = Number(rd.question('insira a segunda nota: '))
 nota3 = Number(rd.question('insira a terceira nota: '))
-avr = Math.floor((nota1+nota2+nota3)/3)
-console.log(`A média das notas é ${avr}.`)
+avrg = Math.floor((nota1+nota2+nota3)/3)
+console.log(`A média das notas é ${avrg}.`)
+*/
+num1 = Number(rd.question('Insira um numero: '))
+num2 = Number(rd.question('Insira outro numero: '))
+if (num1 === num2){
+    comp="é igual a"
+} else if (num1 > num2){
+    comp="é maior que"
+} else if (num1 < num2){
+    comp="é menor que"
+} else {
+    comp=null
+}
+if (comp==null){
+    console.log("ERROR")
+} else {
+    console.log(`O numero ${num1} ${comp} ${num2}`)
+}
