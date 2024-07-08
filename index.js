f6e23c57a1d110a01d42e3a4e0a8856e462f10f8
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-08 11:54:29 -0300

    y

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ v2=v1.at(0).toUpperCase()
 v3=v1.slice(1)
 console.log(v2+v3)
 */
-File(["this is a test"], "./test.txt")
+new File(["this is a test"], "./test.txt")
+/*
+// Function to download data to a file
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+*/
