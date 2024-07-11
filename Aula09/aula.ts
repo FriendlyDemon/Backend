f8f3cfedef76aec9010e9e9763a0ab28b80812c5
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-11 09:58:02 -0300

    y

---
+++ Aula09/aula.ts
@@ -0,0 +1,9 @@
+export class Person {
+  protected children: number;
+  constructor(children: number) {
+    this.children = children;
+  }
+  protected howManyChildren(): void {
+    console.log(`This person has${this.children} children.`);
+  }
+}
