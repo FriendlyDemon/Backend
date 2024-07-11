34c58e84107efc862d067186721a3f1311d9f3c7
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-11 09:11:38 -0300

    y

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const fs = require('node:fs')
 const content = {test1:"hammer",test2:"nail",test3:"board"}
 fs.writeFile('test.json', JSON.stringify(content), err=>{console.error(err)})
 */
-const response =fetch("./test.json")
-const data = response.json
-console.log(data)
+const imp =require("./test.json")
+console.log(imp.test1)
