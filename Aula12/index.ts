28fb98377d5ac82c9d34db4db3bd8ad877ff15cd
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-16 11:38:44 -0300

    y

---
+++ Aula12/index.ts
@@ -0,0 +1,10 @@
+enum DiasDaSemana{
+    Segunda,
+    Terça,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+    Domingo
+}
+for(let i in DiasDaSemana){console.log(i)}
--- index.js
+++ index.js
@@ -9,3 +9,5 @@ console.log(imp.test1)
 // import * as rd from 'readline-sync'
 // number.toFixed(x) = number até o x número depois da ,
 // npm install -D @types/node
+// enum //conteudo /index// conteudo = number /++1// conteudo = string /preencher todos
+// for/ for of/ for in/ forEach()
