c207cc20bbbafc3436b1cd34e279a1757d31a525
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-17 09:26:55 -0300

    y

---
+++ Aula13/index.js
@@ -0,0 +1,11 @@
+for (var i = 10; i >= 0; i--) {
+    if (i % 2 == 0 && i > 1) {
+        console.log("TIC");
+    }
+    else if (i != 0) {
+        console.log("TAC");
+    }
+    else {
+        console.log("BOOM");
+    }
+}
---
+++ Aula13/index.ts
@@ -0,0 +1,5 @@
+for(let i=10;i>=0;i--){
+    if (i%2==0&&i>1){console.log("TIC")}
+    else if (i!=0){console.log("TAC")}
+    else{console.log("BOOM")}
+}
