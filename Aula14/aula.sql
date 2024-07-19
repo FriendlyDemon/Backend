8ad07c390a7e2227db06103fcada7549cfd9f1f7
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-07-19 10:39:59 -0300

    y

--- Aula14/aula.sql
+++ Aula14/aula.sql
@@ -1,50 +1,50 @@
 USE ti3k5791_henriquemichel;
-CREATE TABLE clientes 
+CREATE TABLE Clientes 
 ( 
- id_clientes INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- nome VARCHAR(n) NOT NULL,  
- e_mail INT NOT NULL,  
- telephone VARCHAR(n) NOT NULL,  
- endereco VARCHAR(n),  
- UNIQUE (id_Clientes,E_mail)
+ id_clientes INT PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(100) NOT NULL,  
+ e_mail VARCHAR(100) NOT NULL,  
+ telephone VARCHAR(20) NOT NULL,  
+ endereco VARCHAR(100),  
+ UNIQUE (e_mail)
 ); 
 
-CREATE TABLE produtos 
+CREATE TABLE Produtos 
 ( 
- id_produtos INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- idcategoria INT,  
- nome VARCHAR(n) NOT NULL,  
+ id_produtos INT PRIMARY KEY AUTO_INCREMENT,  
+ idCategoria INT,  
+ nome VARCHAR(100) NOT NULL,  
  preco FLOAT NOT NULL,  
- estoque INT NOT NULL,  
- UNIQUE (id_produtos)
+ estoque INT NOT NULL  
 ); 
 
-CREATE TABLE categoria 
+CREATE TABLE Categoria 
 ( 
- id_categoria INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- nome VARCHAR(n) NOT NULL,  
- UNIQUE (id_categoria)
+ id_categoria INT PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(100) NOT NULL  
 ); 
 
-CREATE TABLE pedidos 
+CREATE TABLE Pedidos 
 ( 
- id_pedidos INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
+ id_pedidos INT PRIMARY KEY AUTO_INCREMENT,  
  idclientes INT,  
- data VARCHAR(n) NOT NULL,  
- total FLOAT NOT NULL,  
- UNIQUE (id_pedidos)
+ data VARCHAR(20) NOT NULL , 
+ total FLOAT NOT NULL 
 ); 
 
-CREATE TABLE pedido_items 
+CREATE TABLE Pedido_Items 
 ( 
- id_pedido_itens INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
+ id_pedido_itens INT PRIMARY KEY AUTO_INCREMENT,  
  idprodutos INT,  
  idpedidos INT,  
- quantidade INT NOT NULL,  
- UNIQUE (id_pedido_itens)
+ quantidade INT NOT NULL  
 ); 
 
-ALTER TABLE produtos ADD FOREIGN KEY(idcategoria) REFERENCES categoria (idcategoria);
-ALTER TABLE pedidos ADD FOREIGN KEY(idClientes) REFERENCES Clientes (idClientes);
-ALTER TABLE pedido_items ADD FOREIGN KEY(idprodutos) REFERENCES produtos (idprodutos);
-ALTER TABLE pedido_items ADD FOREIGN KEY(idpedidos) REFERENCES pedidos (idpedidos);
+-- ALTER TABLE Produtos ADD FOREIGN KEY(idcategoria) REFERENCES Categoria (idcategoria);
+-- ALTER TABLE Pedidos ADD FOREIGN KEY(idclientes) REFERENCES Clientes (idclientes);
+-- ALTER TABLE Pedido_Items ADD FOREIGN KEY(idprodutos) REFERENCES Clientes (idprodutos);
+-- ALTER TABLE Pedido_Items ADD FOREIGN KEY(idpedidos) REFERENCES Clientes (idpedidos);
+
+-- ALTER TABLE "x" ADD "y" INT NOT NULL;
+-- ALTER TABLE "x" CHANGE COLUMN "y" "z" VARCHAR(100);
+-- ALTER TABLE "x" DROP COLUMN "y";
