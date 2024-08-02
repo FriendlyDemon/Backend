da483153237b39837912f5f8803c329b2025bc0d
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-08-02 10:54:12 -0300

    y

--- Aula22/Aula.sql
+++ Aula22/Aula.sql
@@ -15,3 +15,93 @@ BEGIN
     RETURN salario1;
 END //
 DELIMITER ;
+
+-- 1
+
+DELIMITER //
+CREATE FUNCTION CalcularSalarioAnual(
+    cargo_id INT
+)
+RETURNS DECIMAL(10,2)
+BEGIN
+    DECLARE salario1 DECIMAL(10,2);
+    
+    SELECT salario * 12
+    INTO salario1
+    FROM cargos
+    WHERE cargos_id = cargo_id;
+    RETURN salario1;
+END //
+DELIMITER ;
+
+-- 2
+
+DELIMITER //
+CREATE FUNCTION CalcularEmpregadosDepartamento(
+    dep_id INT
+)
+RETURNS INT
+BEGIN
+    DECLARE departamento1 INT;
+    
+    SELECT count(departamento_id)
+    INTO departamento1
+    FROM empregados
+    WHERE departamento_id = dep_id;
+    RETURN departamento1;
+END //
+DELIMITER ;
+
+-- 3
+
+DELIMITER //
+CREATE FUNCTION NomeDepartamento(
+    dep_id INT
+)
+RETURNS VARCHAR(20)
+BEGIN
+    DECLARE depnome1 VARCHAR(20);
+    
+    SELECT nome
+    INTO depnome1
+    FROM departamentos
+    WHERE departamento_id = dep_id;
+    RETURN depnome1;
+END //
+DELIMITER ;
+
+-- 4
+
+DELIMITER //
+CREATE FUNCTION TituloEmpregado(
+    emp_id INT
+)
+RETURNS VARCHAR(20)
+BEGIN
+    DECLARE cargonome VARCHAR(20);
+    
+    SELECT titulo
+    INTO cargonome
+    FROM empregados
+    WHERE empregados_id = emp_id;
+    RETURN cargonome;
+END //
+DELIMITER ;
+
+--5
+
+DELIMITER //
+CREATE FUNCTION SalarioDep(
+    dep_id INT
+)
+RETURNS DECIMAL(10,2)
+BEGIN
+    DECLARE salariodep DECIMAL(10,2);
+    
+    SELECT SUM(salario)
+    INTO salariodep
+    FROM cargos
+    WHERE departamento_id = dep_id;
+    RETURN salariodep;
+END //
+DELIMITER ;
